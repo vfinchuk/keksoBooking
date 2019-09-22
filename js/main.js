c133bfd121ed20e95dd13106536cd6f46d1cff5a
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-09-23 00:53:43 +0300

    Create cardFragment(card) function

--- js/main.js
+++ js/main.js
@@ -103,11 +103,10 @@ function generateCard(amount) {
   return cards;
 }
 
-
 /**
  * Build mapPins fragment array
  * @param {array} cards - cards array
- * @return {Array} DOM elements collection
+ * @return {DocumentFragment} - collection of mapPin elements
  */
 function mapPinsFragment(cards) {
   var fragment = document.createDocumentFragment();
@@ -130,6 +129,35 @@ function mapPinsFragment(cards) {
   });
 
   return fragment;
+}
+
+/**
+ * Build card fragment
+ * @param {object} card - card object
+ * @return {Node} - card__popup element
+ */
+function cardFragment(card) {
+  var template = document.querySelector('#card').content.querySelector('.map__card');
+
+  template.querySelector('.popup__title').textContent = card.offer.title;
+  template.querySelector('.popup__text--address').textContent = card.offer.address();
+  template.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  template.querySelector('.popup__type').textContent = card.offer.type;
+  template.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  template.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  template.querySelector('.popup__features').textContent = card.offer.features.join(', ');
+  template.querySelector('.popup__description').textContent = card.offer.description;
+  template.querySelector('.popup__photos').textContent = '';
+  template.querySelector('.popup__avatar').setAttribute('src', card.author.avatar);
+
+  card.offer.photos.forEach(function (photo) {
+    var image = document.createElement('img');
+    image.setAttribute('src', photo);
+
+    template.querySelector('.popup__photos').appendChild(image);
+  });
+
+  return template;
 }
 
 MAP.classList.remove('map--faded');
