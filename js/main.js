d4c7526739ebd18ad20108caf0f81abd95cbeee3
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-09-22 23:54:07 +0300

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,104 +1,140 @@
 'use strict';
 
-var ORDER_AMOUNT = 8;
-var ORDER_TITLES = ['Люкс аппортаменты', '2х комнатная кв.', 'Комната с видом на море', 'Большая студия', 'Коммуналка', 'Комната с терассой', 'Квартира в центре', 'Уютный Лофт'];
-
-var TYPES_APARTMENT = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
-var CHECKOUT_TIMES = CHECKIN_TIMES;
-
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var CARD_DATA = {
+  amount: 8,
+  titles: [
+    'Люкс аппортаменты',
+    '2х комнатная кв.',
+    'Комната с видом на море',
+    'Большая студия',
+    'Коммуналка',
+    'Комната с терассой',
+    'Квартира в центре',
+    'Уютный Лофт'
+  ],
+  apartments: ['palace', 'flat', 'house', 'bungalo'],
+  checkIns: ['12:00', '13:00', '14:00'],
+  checkOuts: function () {
+    return this.checkIns;
+  },
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  photos: [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ]
+};
 
 var MAP = document.querySelector('.map');
 
-// Return random item in array
-function getRandItem(array) {
+/**
+ * Return random item from array
+ * @param {Array} array - array
+ * @return {Array} - new array
+ */
+function randomArrayItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-// Return random number in diapason
-function getRandomArbitrary(min, max) {
+/**
+ * Return random number in diapason
+ * @param {Number} min - min diapason
+ * @param {Number} max - max max diapason
+ * @return {Number} - random number
+ */
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Return avatar path
-function getAvatarUrl(i) {
-  return 'img/avatars/user0' + i + '.png';
-}
+/**
+ * Return array with random element and length
+ * @param {Array} array - array
+ * @return {Array} - new array
+ */
+function randomItemsLengthArray(array) {
+  var result = [];
+  var length = Math.floor(Math.random() * array.length + 1);
 
-// Return random items and length array
-function getRandSliceArray(array) {
-
-  var j; var x; var i;
-  for (i = array.length - 1; i > 0; i--) {
-
-    j = Math.floor(Math.random() * (i + 1));
-    x = array[i];
-    array[i] = array[j];
-    array[j] = x;
+  for (var i = 0; i < length; i++) {
+    var key = Math.floor(Math.random() * (array.length - 1));
+    result[i] = array[key];
+    array.splice(key, 1);
   }
-  return array.slice(Math.floor(Math.random() * (array.length - 1) + 1));
+
+  return result;
 }
 
-// Generate Orders objects array
-function getOrders() {
-  var orders = [];
-  for (var i = 0; i < ORDER_AMOUNT; i++) {
+/**
+ * Generate array with card objects
+ * @param {Number} amount - cards amount
+ * @return {Array} - cards array
+ */
+function generateCard(amount) {
+  var cards = [];
+  amount = amount < CARD_DATA.titles.length ? amount : CARD_DATA.titles.length;
 
-    orders.push({
+  for (var i = 0; i < amount; i++) {
+
+    cards[i] = {
       author: {
-        avatar: getAvatarUrl(i + 1)
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
-        title: getRandItem(ORDER_TITLES),
-        address: '',
-        price: getRandomArbitrary(300, 1000),
-        type: getRandItem(TYPES_APARTMENT),
-        rooms: getRandomArbitrary(1, 5),
-        guests: getRandomArbitrary(1, 5),
-        checkin: getRandItem(CHECKIN_TIMES),
-        checkout: getRandItem(CHECKOUT_TIMES),
-        features: getRandSliceArray(FEATURES),
+        title: randomArrayItem(CARD_DATA.titles),
+        address: function () {
+          return this.location.x + ', ' + this.location.y;
+        },
+        price: randomNumber(300, 1000),
+        type: randomArrayItem(CARD_DATA.apartments),
+        rooms: randomNumber(1, 5),
+        guests: randomNumber(1, 5),
+        checkin: randomArrayItem(CARD_DATA.checkIns),
+        checkout: randomArrayItem(CARD_DATA.checkOuts()),
+        features: randomItemsLengthArray(CARD_DATA.features),
         description: 'Here will be description',
-        photos: getRandSliceArray(PHOTOS),
+        photos: randomItemsLengthArray(CARD_DATA.photos),
         location: {
-          x: getRandomArbitrary(MAP.offsetLeft, MAP.offsetWidth),
-          y: getRandomArbitrary(130, 630)
+          x: randomNumber(MAP.offsetLeft, MAP.offsetWidth),
+          y: randomNumber(130, 630)
         }
       }
-    });
-
+    };
   }
-
-  return orders;
+  return cards;
 }
 
-// Rendering map_pin buttons
-function renderMapPins(orders, list) {
-  var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-  for (var i = 0; i < orders.length; i++) {
-    var pinElement = mapPinTemplate.cloneNode(true);
+/**
+ * Build mapPins fragment array
+ * @param {array} cards - cards array
+ * @return {Array} DOM elements collection
+ */
+function mapPinsFragment(cards) {
+  var fragment = document.createDocumentFragment();
+  var template = document.querySelector('#pin').content.querySelector('.map__pin');
 
-    pinElement.querySelector('img').setAttribute('src', orders[i].author.avatar);
-    pinElement.querySelector('img').setAttribute('alt', orders[i].offer.title);
+  cards.forEach(function (card) {
+    var pinElement = template.cloneNode(true);
+    var pinElementImage = pinElement.querySelector('img');
 
-    list.appendChild(pinElement);
+    pinElement.querySelector('img').setAttribute('src', card.author.avatar);
+    pinElement.querySelector('img').setAttribute('alt', card.offer.title);
 
-    var pinImage = pinElement.querySelector('img');
-    var locX = orders[i].offer.location.x - (pinElement.offsetWidth / 2);
-    var locY = orders[i].offer.location.y - pinImage.offsetHeight;
 
+    var locX = card.offer.location.x - pinElementImage.getAttribute('width');
+    var locY = card.offer.location.y - pinElementImage.getAttribute('height');
     pinElement.style.left = locX + 'px';
     pinElement.style.top = locY + 'px';
-  }
 
+    fragment.appendChild(pinElement);
+  });
+
+  return fragment;
 }
 
 MAP.classList.remove('map--faded');
 
-var mapPinsList = document.querySelector('.map__pins');
-var orders = getOrders();
+var cards = generateCard(12);
+var mapPinsWrap = document.querySelector('.map__pins');
 
-renderMapPins(orders, mapPinsList);
+mapPinsWrap.appendChild(mapPinsFragment(cards));
