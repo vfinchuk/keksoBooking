72f95c6174ce634e67ac9be169902bb65339ecc1
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-09-30 19:20:56 +0300

    Add close/open event for map__card popup

--- js/map.js
+++ js/map.js
@@ -2,15 +2,11 @@
 
 (function () {
 
-  // var MAP = window.util.BOOK_MAP;
-  // var FORM = window.util.BOOK_FORM;
-  // var FILTERS = window.util.BOOK_FILTERS;
+  var MAP = window.util.BOOK_MAP;
 
+  var mapPinsWrap = MAP.querySelector('.map__pins');
   var mapFiltersWrap = window.util.BOOK_MAP.querySelector('.map__filters-container');
   var cards = window.generateCard(12);
-
-  mapFiltersWrap.insertAdjacentElement('beforebegin', window.fragments.cardFragment(cards[2]));
-
 
   /**
    * Handler for close card popup
@@ -21,12 +17,57 @@
   }
 
 
-  addEventListener('DOMContentLoaded', function () {
+  /**
+   * Toggle for show popup card
+   */
+  function togglePopupCardOrder() {
+    var mapPins = MAP.querySelectorAll('.map__pin');
 
-    var mapCardCloseBtn = document.querySelector('.popup__close');
-    mapCardCloseBtn.addEventListener('click', __closeMapCardHandler);
+    mapPins.forEach(function (pin) {
+
+      if (!pin.classList.contains('map__pin--main')) {
+        pin.addEventListener('click', function () {
+
+          var mapCard = document.querySelector('.map__card');
+          if (mapCard) {
+            document.querySelector('.popup__close').removeEventListener('click', __closeMapCardHandler);
+            __closeMapCardHandler();
+          }
+
+          var pinTitle = pin.querySelector('img').getAttribute('alt');
+
+          for (var i = 0; i < cards.length; i++) {
+            if (pinTitle === cards[i].offer.title) {
+              mapFiltersWrap.insertAdjacentElement('beforebegin', window.fragments.cardFragment(cards[i]));
+            }
+          }
+          document.querySelector('.popup__close').addEventListener('click', __closeMapCardHandler);
+        });
+      }
+    });
+  }
+
+  /**
+   * Event close card__popup by esc button
+   */
+  addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.util.ESC_BTN && document.querySelector('.map__card')) {
+      __closeMapCardHandler();
+    }
   });
 
+  /**
+   * Render map pins fragment
+   */
+  function renderMapPins() {
+    mapPinsWrap.appendChild(window.fragments.mapPinsFragment(cards));
+  }
+
+
+  window.map = {
+    togglePopupCardOrder: togglePopupCardOrder,
+    renderMapPins: renderMapPins
+  };
 
   // ...
 
