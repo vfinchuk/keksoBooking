5ca9037e9fedb4ecbc55c921ebafe47090e2b8c3
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-10-02 22:10:08 +0300

    Create movingElementOnMap function

--- js/pin.js
+++ js/pin.js
@@ -4,7 +4,8 @@
 
   var mapNode = window.util.mapNode;
   var mapPinsWrap = mapNode.querySelector('.map__pins');
-
+  var mapFiltersWrap = mapNode.querySelector('.map__filters-container');
+  var mapPinMain = mapNode.querySelector('.map__pin--main');
 
   /**
    * Build mapPins fragment array
@@ -33,6 +34,60 @@
 
     return fragment;
   }
+
+
+  /* moving main map */
+
+  function movingElementOnMap(nodeElement) {
+    nodeElement.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+
+      function __mouseMoveHandler(moveEvt) {
+        moveEvt.preventDefault();
+
+        var elWidthOffset = nodeElement.clientWidth / 2;
+        var elHeightOffset = nodeElement.clientHeight / 2;
+
+        var shift = {
+          x: moveEvt.clientX - mapNode.offsetLeft - elWidthOffset,
+          y: moveEvt.clientY - mapNode.offsetTop - elHeightOffset
+        };
+
+        /* moving on map by x-coordinate */
+        if (
+          (moveEvt.clientX < (mapNode.offsetLeft + elWidthOffset)) ||
+          (moveEvt.clientX > (mapNode.offsetLeft + mapNode.clientWidth - elWidthOffset))
+        ) {
+          nodeElement.style.left = (mapNode.offsetLeft + elWidthOffset);
+        } else {
+          nodeElement.style.left = shift.x + 'px';
+        }
+
+        /* moving on map by y-coordinate */
+        if (moveEvt.clientY < (mapNode.offsetTop + elHeightOffset)) {
+          nodeElement.style.top = mapNode.offsetTop + mapNode.scrollTop + 'px';
+        } else if (moveEvt.clientY > (mapNode.offsetTop + mapNode.clientHeight - mapFiltersWrap.clientHeight)) {
+          nodeElement.style.top = (mapNode.offsetTop + mapNode.clientHeight - mapFiltersWrap.clientHeight) - nodeElement.clientHeight + mapNode.scrollTop + 'px';
+        } else {
+          nodeElement.style.top = shift.y + mapNode.scrollTop + 'px';
+        }
+
+      }
+
+      function __mouseUpHandler() {
+        document.removeEventListener('mousemove', __mouseMoveHandler);
+        document.removeEventListener('mouseup', __mouseUpHandler);
+      }
+
+      document.addEventListener('mousemove', __mouseMoveHandler);
+      document.addEventListener('mouseup', __mouseUpHandler);
+
+    });
+  }
+
+
+  movingElementOnMap(mapPinMain);
 
   /* Render map pins fragment */
   function renderMapPins() {
